e47cecddc31189b1b60b45ac7d971ee2ab2dd73e
Yoganand Parab <yoganandparab@gmail.com>
2019-08-11 11:16:44 +0530

    *start

--- Movie_Ticket_Booking.cpp
+++ Movie_Ticket_Booking.cpp
@@ -3,31 +3,8 @@
 
 #include <iostream>
 #include <string.h>
-#include "date_n_time.h"
-#include "language.h"
 
 using namespace std;
-
-class Seat {
-	char letter;
-	int num;
-public:
-	Seat():letter(' '),num(0){}
-	Seat(char l, int n):letter(l),num(n){}
-};
-
-class T_info {
-	char movie_name[25], location[10];
-	int no_of_seats;
-	Language language;
-	Seat seat[10];
-	time tm;
-	date dt;
-};
-
-int main(){
-    std::cout << "Hello World!\n";
-}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
